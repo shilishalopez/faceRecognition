a6432bb2aad2c05f05b4b967c194c5044c725cc0
Shilisha Lopez <shilisha.lopez4@gmail.com>
2023-03-24 22:18:39 -0400

    changing server location

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ class App extends Component{
     .then(response => {
         console.log('hi', response)
         if(response){
-          fetch('http://localhost:3001/image',{
+          fetch('https://face-recognition-backend-29d4.onrender.com',{
             method: 'put',
             headers:{'Content-Type': 'application/json'},
             body: JSON.stringify({
